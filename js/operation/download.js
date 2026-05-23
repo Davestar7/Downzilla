02b7d49d69bd3e9a5858baddbfa4008deb46e58d
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-23 21:01:53 +0100

    Update download.js

--- js/operation/download.js
+++ js/operation/download.js
@@ -348,6 +348,9 @@ async function downloadVideo(outurl, title, start, end, format, from = null, hea
             credentials: "include"
         }).then(r => r.json());
 
+       console.log(success)
+       console.log(jobId)
+
         if (!success || !jobId) {
             alert("download failed, please try again");
             btn.innerHTML = "download mp4 video";
