d4e3bc4f9da5a690fd9e88f203ef89924e902a77
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-23 11:15:31 +0100

    Update streamvideo.js

--- js/operation/streamvideo.js
+++ js/operation/streamvideo.js
@@ -221,7 +221,7 @@ function generateThumbnailContainer(containerPath, thumbnailUrl) {
   const html = `
     <div class="gc-card" style="
       width: 100%;
-      height: 30vh;
+      height: 40vh;
       min-height: inherit;
       position: relative;
       overflow: hidden;
