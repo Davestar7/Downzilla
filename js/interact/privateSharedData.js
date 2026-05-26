1bc53a25f884ce0cbc683e2f6f7c43e7e6b8ee94
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-26 10:31:03 +0100

    Update privateSharedData.js

--- js/interact/privateSharedData.js
+++ js/interact/privateSharedData.js
@@ -2,12 +2,12 @@ import { userData } from "../auth/afterauth.js";
 import historyPage from "../../UI-components/historyData.js"
 
 function main() {
-    const url = window.location.pathname
-    const plist = url.split("/")
-    const user_id = plist[plist.length-2]
-    const history_id = plist[plist.length-1]
+    const url = window.location.pathname;
+    const plist = url.split("/").filter(Boolean);
+    const user_id = plist[plist.length - 2];
+    const history_id = plist[plist.length - 1];
 
-    historyPage(user_id, history_id)
+    historyPage(user_id, history_id);
 }
 
 export default main
