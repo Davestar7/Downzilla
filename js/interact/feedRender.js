17cc10f868609a5dfae38e6a6a71d0a694e5f2da
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-05-27 07:55:51 +0100

    Update feedRender.js

--- js/interact/feedRender.js
+++ js/interact/feedRender.js
@@ -32,7 +32,7 @@ async function handleFeedDisplay(newPage = null) {
 
         foot.innerHTML = `<ul id="pagenumb"></ul>`
         const footer = document.getElementById("pagenumb")
-
+        footer.innerHTML = <div id="rnavdiv"></div>
         if (totalContainablePage === null) {
             getContent()
         } else {
@@ -54,7 +54,7 @@ async function handleFeedDisplay(newPage = null) {
                                     `
                     }
                 }
-                document.getElementById("pagenumb").innerHTML = navStr
+                document.getElementById("rnavdiv").innerHTML = navStr
                 changeRenderingPageNumber(curentPage)
             }
         }
