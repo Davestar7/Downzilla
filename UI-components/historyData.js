e7a20a2102797fce407097c11a7ab26e71079c84
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-06-11 16:49:03 +0100

    Update historyData.js

--- UI-components/historyData.js
+++ UI-components/historyData.js
@@ -134,7 +134,7 @@ async function updateData(id, isPublic, type, cid) {
         return
     }
 
-    historyRender(data, isPublic, type, cid)
+    historyRender(data, isPublic, type, null, false, cid)
     document.getElementById("sharefeed").addEventListener("click", () => {
             share("private", {one: userData.userId, two: cid}, data.data.title)
         })
