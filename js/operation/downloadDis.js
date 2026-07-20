87163e9e3d8ffc800ffa730029bd9518478ef784
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-07-20 09:53:09 +0100

    Correct type check for seconds in timeformat
    Fix type check for seconds in timeformat function.

--- js/operation/downloadDis.js
+++ js/operation/downloadDis.js
@@ -6,7 +6,7 @@ import { closeFunction } from "../alert.js"
 import { uploadHistory, uploadContent } from "../interact/history.js"
 
 function timeformat(sec) {
-    if (typeof sec === Number) return "..."
+    if (typeof sec !== Number) return "..."
     const h = Math.floor(sec / 3600).toString().padStart(2, "0")
     const m = Math.floor((sec % 3600) / 60). toString().padStart(2, "0")
     const s = Math.floor(sec % 60).toString().padStart(2, "0")
