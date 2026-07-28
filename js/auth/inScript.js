515c1151ff3d758e2392359da86ca179e6094326
Davestar7 <davidnyeokpobiyadavid3633@gmail.com>
2026-07-28 18:55:01 +0100

    Refactor handleCredentialResponse for clarity and fixes

--- js/auth/inScript.js
+++ js/auth/inScript.js
@@ -3,30 +3,23 @@
 // what do you mean there is bug is working fine trust me;
 
 async function handleCredentialResponse(response) {
-    // Decode the JWT
-    console.log("google token: ", response.credential)
-        const credentials = JSON.parse(response)
-
-        try {
-           const googleAuth = await fetch("https://downzilla-backend.onrender.com/auth/googleauth/callback", {
-                method: "POST",
-                headers: {"content-Type": "application/json"},
-                body: JSON.stringify({credential: credentials}),
-                credentials: "include"
-           })
-           const datas = googleAuth;
-           const data = await datas.json()
-           
-           if (data.success === true) {
-                const accessToken = data.accessT
-                localStorage.setItem("DZAT", accessToken)
-                
-                window.location.reload()
-            } else {
-                alert("something when wong \n please try again or \n or try another method")
-                window.location.reload()
-            }
-        } catch (e) {
-           alert("<em>error connecting, please check internet connection</em>")
+    console.log("google token: ", response.credential);
+    try {
+        const googleAuth = await fetch("https://downzilla-backend.onrender.com/auth/googleauth/callback", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ credential: response.credential }),
+            credentials: "include"
+        });
+        const data = await googleAuth.json();
+        if (data.success === true) {
+            localStorage.setItem("DZAT", data.accessT);
+            window.location.reload();
+        } else {
+            alert("Something went wrong, please try again or try another method");
         }
+    } catch (e) {
+        console.error(e);
+        alert("Error connecting, please check internet connection");
     }
+}
